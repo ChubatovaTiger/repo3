3f519ef5a7ae12a6100aa094d6a50d368849bd1d
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2023-12-14 14:22:50 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,28 +3,6 @@ import jetbrains.buildServer.configs.kotlin.buildFeatures.parallelTests
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2023.11"
 
 project {
@@ -39,7 +17,7 @@ object Build1 : BuildType({
     name = "build1"
 
     vcs {
-        root(HttpsGithubComChubatovaTigerChubatovaGradleTestsBackup, ".=>folder")
+        root(HttpsGithubComChubatovaTigerChubatovaGradleTestsBackup, ".=>folder2")
         root(DslContext.settingsRoot, "+:. => .a")
     }
 
@@ -48,7 +26,7 @@ object Build1 : BuildType({
             id = "gradle_runner"
             tasks = "clean build"
             buildFile = "build.gradle"
-            workingDir = "folder"
+            workingDir = "folder2"
             jdkHome = "%env.JDK_11_0%"
         }
     }
@@ -79,7 +57,7 @@ object Build2 : BuildType({
 
     features {
         parallelTests {
-            numberOfBatches = 2
+            numberOfBatches = 3
         }
     }
 })
