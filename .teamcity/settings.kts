41cc8b2dbbd59f2e96048f9f27bfc22499ffa2b8
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2023-12-14 14:32:50 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,27 +5,7 @@ import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.matrix
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
 
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
 
 version = "2023.11"
 
@@ -125,7 +105,7 @@ object Chain : Project({
     name = "chain"
 
     buildType(Chain_Build1)
-    buildType(Chain_Build2)
+    buildType(Chain_Build3)
 })
 
 object Chain_Build1 : BuildType({
@@ -136,16 +116,22 @@ object Chain_Build1 : BuildType({
     }
 
     dependencies {
-        snapshot(Chain_Build2) {
+        snapshot(Chain_Build3) {
             reuseBuilds = ReuseBuilds.NO
         }
     }
 })
 
-object Chain_Build2 : BuildType({
-    name = "build2"
+object Chain_Build3 : BuildType({
+    name = "build3"
 
     vcs {
         root(HttpsGithubComChubatovaTigerChubatovaGradleTestsBackup, ".=>f2")
+    }
+    steps {
+        script {
+            id = "simpleRunner"
+            scriptContent = "ls f2"
+        }
     }
 })
